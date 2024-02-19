bf776eb9d498b06a36d33827744c6df8aea0b7d7
Natan Rocha <natan.rocha@codeandsoul.com>
2024-02-19 15:04:55 -0300

    Added a logic to change the buttons focus

--- script.js
+++ script.js
@@ -4,42 +4,49 @@ const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
+const botoes = document.querySelectorAll('.app__card-button');
 
 focoBt.addEventListener('click',() =>{
 
     alterarContexto('foco');
+    focoBt.classList.add('active');
 
 })
 curtoBt.addEventListener('click',() =>{
 
     alterarContexto('descanso-curto');
+    curtoBt.classList.add('active');
 
 })
 longoBt.addEventListener('click',() =>{
 
     alterarContexto('descanso-longo');
+    longoBt.classList.add('active');
 
 })
 
 function alterarContexto(contexto) {
+    botoes.forEach((contexto) =>{
+        contexto.classList.remove('active')
+    })
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `/imagens/${contexto}.png`);
     switch (contexto) {
         case 'foco':
             titulo.innerHTML=`
-                    Otimize sua produtividade,<br>
+                Otimize sua produtividade,<br>
                     <strong class="app__title-strong">mergulhe no que importa.</strong>
             `         
             break;
         case 'descanso-curto':
             titulo.innerHTML =`
-                    Que tal dar uma respirada?<br>
+                Que tal dar uma respirada?<br>
                     <strong class="app__title-strong">Faça uma pausa curta!</strong>
             `
         break;
         case 'descanso-longo':
             titulo.innerHTML =`
-                    Hora de voltar à superfície.<br>
+                Hora de voltar à superfície.<br>
                     <strong class="app__title-strong">Faça uma pausa longa.</strong>
             `
         break;
