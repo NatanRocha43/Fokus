154f576a2f1a95d88279ba30cbbc0297e932744d
Natan Rocha <natan.rocha@codeandsoul.com>
2024-02-19 15:53:30 -0300

    Added a button counter logic and added sound to it

--- script.js
+++ script.js
@@ -5,9 +5,20 @@ const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
 const botoes = document.querySelectorAll('.app__card-button');
+const startPauseBT = document.querySelector('#start-pause');
 const musicaFocoInput = document.querySelector('#alternar-musica');
 const musica = new Audio('/sons/luna-rise-part-one.mp3');
+const audioPlay = new Audio('/sons/play.wav')
+const audioPause = new Audio('/sons/pause.mp3')
+const audioTempoFinalizado = new Audio('./sons/beep.mp3')
+
+
+let tempoDecorridoEmSegundos = 5
+let intervaloId = null
+
 musica.loop = true;
+
+
 
 musicaFocoInput.addEventListener('change', () =>{
     musica.paused ? musica.play() : musica.pause();
@@ -61,3 +72,32 @@ function alterarContexto(contexto) {
             break;
     }
 }
+
+const contagemRegressiva = () => {
+    if(tempoDecorridoEmSegundos <= 0) {
+        // audioTempoFinalizado.play();
+        alert('Tempo finalizado');
+        zerar();
+        return;
+    }
+    tempoDecorridoEmSegundos -= 1;
+
+}
+
+function iniciarOuPausar() {
+    if (intervaloId) {
+        audioPause.play(); 
+        zerar();
+        return
+    }
+    audioPlay.play();
+    intervaloId = setInterval(contagemRegressiva, 1000);
+}
+
+
+function zerar(){
+    clearInterval(intervaloId);
+    intervaloId = null;
+}
+
+startPauseBT.addEventListener('click', iniciarOuPausar);
