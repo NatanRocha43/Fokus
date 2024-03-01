1319aea5a6fcd22c3b82c8930448e7c6a5e35790
Natan Rocha <natan.rocha@codeandsoul.com>
2024-03-01 16:21:22 -0300

    Added a task list

--- script.js
+++ script.js
@@ -82,8 +82,13 @@ function alterarContexto(contexto) {
 
 const contagemRegressiva = () => {
     if(tempoDecorridoEmSegundos <= 0) {
-        // audioTempoFinalizado.play();
+        audioTempoFinalizado.play();
         alert('Tempo finalizado');
+        const focoAtivo = html.getAttribute('data-contexto') == 'foco';
+        if (focoAtivo) {
+            const evento = new CustomEvent('FocoFinalizado');
+            document.dispatchEvent(evento)
+        }
         zerar();
         return;
     }
